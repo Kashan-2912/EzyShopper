4590025de6b7061185e4548ac6afb966d5a2cbc7
Muhammad Kashan Ashraf <mkashan2912@gmail.com>
2024-12-23 21:52:54 +0500

    env, server.js, package.json changed

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import express from "express";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 // routes
 import authRoutes from "./routes/auth.route.js"
@@ -18,6 +19,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(express.json({limit: "10mb"})) // allow to parse body of request
 app.use(cookieParser())
 
@@ -27,6 +30,14 @@ app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+    })
+}
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
