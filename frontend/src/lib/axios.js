7290d22f830e428d1f308f803164b0f6fed4406e
Muhammad Kashan Ashraf <mkashan2912@gmail.com>
2026-01-30 01:40:47 +0500

    feat: add backend railway link to axios

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.mode === "development" ? "http://localhost:3000/api" : "/api",
+    baseURL: import.meta.mode === "development" ? "http://localhost:3000/api" : "https://ezyshopper-production.up.railway.app/api",
     withCredentials: true, //allows us to send cookies on every single request to server
 });
 
