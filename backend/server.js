ee267f99ae767b3d4ab4660ffed050e130df8ac1
Muhammad Kashan Ashraf <mkashan2912@gmail.com>
2026-02-18 16:53:42 +0500

    feat: added health check route

--- backend/server.js
+++ backend/server.js
@@ -35,6 +35,10 @@ app.use(express.json({limit: "10mb"})) // allow to parse body of request
 app.use(express.static('public'));
 app.use(cookieParser())
 
+app.get("/api/health", (req, res) => {
+    res.json({ status: "ok" });
+})
+
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
