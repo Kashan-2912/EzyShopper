4e61e663cb456ea850d09f438f0b59f8cd03c132
Muhammad Kashan Ashraf <mkashan2912@gmail.com>
2026-02-18 16:59:55 +0500

    feat: updated axios to call render backend instead railway

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.mode === "development" ? "http://localhost:3000/api" : "https://ezyshopper-production.up.railway.app/api",
+    baseURL: import.meta.mode === "development" ? "http://localhost:3000/api" : "https://ezyshopper.onrender.com/api",
     withCredentials: true, //allows us to send cookies on every single request to server
 });
 
