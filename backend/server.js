12589ccb22fdbe5703df8c5f974f91620d2f21dd
Muhammad Kashan Ashraf <mkashan2912@gmail.com>
2026-02-18 16:59:22 +0500

    feat: health path udpated

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ app.use(express.json({limit: "10mb"})) // allow to parse body of request
 app.use(express.static('public'));
 app.use(cookieParser())
 
-app.get("/api/health", (req, res) => {
+app.get("/", (req, res) => {
     res.json({ status: "ok" });
 })
 
